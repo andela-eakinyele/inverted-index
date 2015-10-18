ee76cd8847dd70095e68e4bcb158be3f0996903b
andela-eakinyele <emmanuel.akinyele@andela.com>
2015-10-18 17:14:49 +0100

    feat : createIndex, getIndex -createIndex creates new index object -getIndex fetches specified index object

--- jasmine/spec/invertedIndexTest.js
+++ jasmine/spec/invertedIndexTest.js
@@ -1,7 +1,18 @@
 describe("Read book data", function(){
+	var Index = require('../../lib/jasmine_examples/invertedIndex');
+	var filepath;
 
-	it("Verify non-empty JSON data",function(){
+	
+beforeEach(function(){
+	index = new Index();
+	filepath = 'books.json';
+});
 
+	it("Verify non-empty JSON data", function(){
+		console.log(index.createIndex(filepath));
+		expect(next).toEqual('error opening json');
+		expect(4).toEqual(4);
+		//expect(next).toBeFalsy();
 	});
 });
 
--- src/version 2.0.js
+++ src/version 2.0.js
@@ -1,7 +1,10 @@
 var fs  = require('fs'), 
 	path = require('path'), async = require('async');
-var index_all = '', tokens = '', 
-	filepaths = '', newdata = '';
+var index_by_keys = ['',''], index_all = '', tokens = '', 
+	filepaths = '', key_count = 0, keys = [], newdata = '';
+
+var args = process.argv.slice(-1);
+var flag = (args.indexOf('-g') >= 0);
 
 function Index(){
 	// variables for serializing indices
@@ -11,44 +14,6 @@ function Index(){
 	this.count = 0;
 }
 
-// creates an index from the JSON data 
-Index.prototype.createIndex = function(filepath){
-	var obj  = this;
-	filepaths = filepath;
-	var new_index = obj.getfilename(filepath);
-	if (obj.indexname.indexOf(new_index) < 0) {
-		obj.indexname.push(new_index);
-		++this.count;
-	}
-	obj.nindexes[new_index] = {};		
-	obj.jsondata[new_index] = {};
-
-	obj.readingFile(filepaths, new_index, 
-		function(index_all){
-			getTokens(index_all);
-			generateTokendata(obj, tokens, obj.jsondata[new_index], new_index);
-			return obj.nindexes[new_index];
-	});
-
-};
-
-// return index 
-Index.prototype.getIndex = function (indexname){	
-	var obj = this;
-		return obj.createIndex(indexname);
-};
-
-// return index 
-Index.prototype.searchIndex = function (terms){	
-	var obj = this;
-	if(terms.length > 0){
-		terms.forEach(function(item){
-			if(obj.nindexes[indexname][item]) console.log;
-		});
-	}
-	else{if(obj.nindexes[indexname][terms]) console.log;}
-};
-
 // obtain name of JSON file for serializing index
 Index.prototype.getfilename = function(filepath){
 	if(filepath !== null){
@@ -57,9 +22,21 @@ Index.prototype.getfilename = function(filepath){
 	else{return null;}
 };
 
+// creates an index from the JSON data 
+Index.prototype.createIndex = function(filepath){
+	filepaths = filepath;
+	var new_index = this.getfilename(filepath);
+	if (this.indexname.indexOf(new_index) < 0) {
+		this.indexname.push(new_index);
+		++this.count;
+	}
+	this.nindexes[new_index] = {};		
+	this.jsondata[new_index] = {};
+};
+
 // reads the content ofthe JSON file, and builds index
 Index.prototype.readingFile = function (filepath, indexname, callback){
-	// binding object 
+	// binding 
 	var obj = this;
 	async.waterfall([
 		function(concats){
@@ -79,30 +56,63 @@ Index.prototype.readingFile = function (filepath, indexname, callback){
 		function(newdata, indexname, concats){
 			obj.jsondata[indexname] = newdata;
 			// initialize variables
-			index_all = '';
+			index_all = ''; index_by_keys = ['']; keys = []; key_count = 0;
+
 			// concatenate content of each object to for building index
 			newdata.forEach(function(item, index){
+
+				// re-initialize key value pair count object
+				key_count = 0;
 				// looping through each object in the data
 				// concatenate data into a single string (index_all)
+				// group by keys in object (index_by Keys)
 				for (var key in item){
 					index_all += ' ' + item[key];
+					index_by_keys[key_count] += ' '+item[key];
+					key_count++;
+					index_by_keys[key_count] = '';		
 				}
+				--key_count;
 			});
 			concats(null);
 		}
-		], function concats(err){
+		], function concats(err, results){
 				if (err) return console.error(err);			
-				callback(index_all);
-  });	
+				callback(index_all, index_by_keys);
+  });
+	
 };
 
+Index.prototype.getIndex = function (indexname){	
+	var obj = this;
+	obj.readingFile(filepaths, indexname, 
+		function(index_all, index_by_keys){
+			getTokens(index_all, index_by_keys);
+			generateTokendata(obj, tokens, obj.jsondata[indexname], indexname);
+			console.log(obj.nindexes[indexname]);
+	});
+};
 
-function getTokens(index_all){
-	var tokens_i;	
+function getTokens(index_all, index_by_keys){
+	var tokens_i;
+	
 	// for full text search
+	if(!flag){
 		tokens ='';
+		console.log("executed no flag");
 		tokens_i = index_all.toLowerCase().split(/[.,\s]+/); //split by period, comma and spaces
 		tokener(tokens_i);
+	}
+	// for group index search
+	else{
+		tokens = [];
+		console.log("executed flag");
+		index_by_keys.forEach(function(item, index){
+			if(item !== ''){
+			tokens_i = item.toLowerCase().split(/[.,\s]+/);
+			tokener(tokens_i);}
+		});		
+	}
 }
 
 // sorts concatenated data and collect unique tokens
@@ -118,6 +128,7 @@ function tokener(tokens_i){
 
 // generates index by group or full index
 function generateTokendata(obj, tokens, jsondata, indexname){
+	if(!flag){
 		tokens.forEach(function(item, index){
 			obj.nindexes[indexname][item] = [];		
 			for(var keys in jsondata){
@@ -133,6 +144,31 @@ function generateTokendata(obj, tokens, jsondata, indexname){
 		});
 	}
 
-index1 = new Index();
+	else
+	{
+		for(var key1 in jsondata){
+			var mapkey = 0;
+			for(var key2 in jsondata[key1]){
+				if(obj.nindexes[indexname][key2] === undefined){obj.nindexes[indexname][key2] = {};}
+					tokens[mapkey].forEach(function(item){
+						if(obj.nindexes[indexname][key2][item] == undefined){
+							obj.nindexes[indexname][key2][item] = [];
+						}
+						var Str = jsondata[key1][key2].toLowerCase();
+						if (Str.indexOf(item) !== -1){
+							obj.nindexes[indexname][key2][item].push(key1);
+						}	
+					});	
+					mapkey++;						
+			} 
+		}
+	}
 
-index1.getIndex('../../spec/jasmine_examples/books.json');
+}
+index1 = new Index();
+index1.createIndex('../../spec/jasmine_examples/books.json');
+index1.getIndex('books');
+index1.createIndex('../../spec/jasmine_examples/users.json');
+index1.getIndex('users');
+index1.getIndex('books');
+
