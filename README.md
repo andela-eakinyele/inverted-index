453626ee1d8ac8eb7704a9da389e72809d3d2cfe
andela-eakinyele <emmanuel.akinyele@andela.com>
2015-10-22 22:03:34 +0100

    Refactor code
    -Used ajax call to read file
    -Verified jasmine tests

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # inverted-index
+This is a simple application used to generate index of json formatted files
+It enables a quick search of words based on the index generated.
+
+## Usage
+The usage of the application is as follows:
+
+### Creating Index
+1.  Create a new Index instance using the "new constructor" e.g new Index();
+2.  Call the createIndex(filepath) method of the instance passing in the json filepath as argument
+3.  Call the getIndex(filename) to see the index generated
+
+### Search
+####  Full index search
+  A full index search searches through all generated indices for all json file
+    This is executed by calling the *searchIndex(terms)*
+
+####  Index specifiers
+  Index specifier restricts searches to specific indices, *only one* specifier is implemented, further development will include multiple specifiers
+  Index specifiers utilize the format in:indexname
+
+####  Search terms
+  Search may include either:
+  - strings containing one or more words
+  - Array of words
+  Note that to use specifiers in array, the specifier is must be the first element in the array
+    e.g ['in:books', 'Alice', 'powerful']
+  For string the specifier may occur anywhere in the search string
+  e.g 'alice in:users powerful'  or "alice powerful in:users"
--- src/invertedIndex.js
+++ src/invertedIndex.js
@@ -1,138 +1,102 @@
-var fs  = require('fs'), 
-	path = require('path'), async = require('async');
-var index_all = '', tokens = '', 
-	filepaths = '', newdata = '';
+var index_all = '', tokens = '',  newdata = '';
 
-function Index(){
-	// variables for serializing indices
-	this.indexname = [];
-	this.jsondata = {};
-	this.nindexes = {};
-	this.count = 0;
+function Index() {
+  // variables for serializing indices
+  this.indexname = [];
+  this.jsondata = {};
+  this.nindexes = {};
+  this.count = 0;
+  // creates an index from the JSON data 
+  this.createIndex = function(filepath) {
+    // initialize variables for index
+    var obj = this;
+    var new_index = obj.getfilename(filepath);
+    if (obj.indexname.indexOf(new_index) < 0) {
+        obj.indexname.push(new_index);
+        ++this.count;
+    }
+    obj.nindexes[new_index] = {};
+    obj.jsondata[new_index] = {};
+    // read file and generate index
+    obj.readjson(filepath, new_index)
+    getTokens(index_all);
+    generateTokendata(obj, tokens, obj.jsondata[new_index], new_index);
+  };
+  // return index 
+  this.getIndex = function(filepath) {
+    var obj = this;
+    var index = obj.getfilename(filepath);
+    if (obj.indexname.indexOf(index) < 0) {
+        return index + " index does not exist";
+    }
+    return obj.nindexes[index];
+  };
+  // return search results 
+  this.searchIndex = function(terms) {
+    var obj = this, flag, indexname;
+    // check for flag for index object specifier
+    if (typeof terms === 'object') {
+      flag = terms[0].search(/in:\s*\w+/);
+      if (flag > -1) {
+        indexname = (/in:\s*(\w+)/).exec(terms[0])[1];
+        terms = terms.slice(1);
+      }
+    }
+    // parse string into array
+    else {
+      flag = terms.search(/in:\s*\w+/);
+      if (flag > -1) indexname = (/in:\s*(\w+)/).exec(terms)[1];
+
+      terms = terms.replace(/in:\w+/, '').trim().split(/[.,\s*]+/);
+    }
+    // use flag to search specified index
+    if (flag > -1) {
+      if (obj.nindexes[indexname] == undefined) {
+        return indexname + ' index does not exist';
+      }
+      return searching(obj, terms, indexname);
+    }
+    // search in all indices
+    else {
+      var result_all = {};
+      for (var key in obj.nindexes) {
+        result_all[key] = {};
+        result_all[key] = searching(obj, terms, key);
+	    }
+      return result_all;
+    }
+  };
+  // obtain name of JSON file for serializing index
+  this.getfilename = function(filepath) {
+    if (filepath !== undefined && filepath !== '') {
+      var path = filepath.split(/[.\/]+/);
+      return path[path.length - 2];
+    } else {
+	      return null;
+    }
+  };
+  // reads the content ofthe JSON file, and builds index
+  this.readjson = function(filepath, indexname) {
+    // binding object
+    var obj = this;
+      // ajax call to read json file
+    $.ajax({
+      dataType: "json",
+      async: false,
+      url: filepath,
+      success: function(data) {newdata = data;},
+      error: function(err) { return err.message;}
+    });
+    if(newdata === ''){return 'empty json';}
+    // writing data to index instance
+    obj.jsondata[indexname] = newdata;
+    // initialize variables
+    index_all = '';
+    // concatenate content of each object to for building index
+    newdata.forEach(function(item, index) {
+      for (var key in item) {
+        index_all += ' ' + item[key];
+      }
+    });
+  };
 }
-
-// creates an index from the JSON data 
-Index.prototype.createIndex = function(filepath){
-	var obj  = this;
-	filepaths = filepath;
-	var new_index = obj.getfilename(filepath);
-	if (obj.indexname.indexOf(new_index) < 0) {
-		obj.indexname.push(new_index);
-		++this.count;
-	}
-	obj.nindexes[new_index] = {};		
-	obj.jsondata[new_index] = {};
-
-	obj.readingFile(filepaths, new_index, 
-		function(index_all){
-			getTokens(index_all);
-			generateTokendata(obj, tokens, obj.jsondata[new_index], new_index);
-			return obj.nindexes[new_index];
-	});
-
-};
-
-// return index 
-Index.prototype.getIndex = function (indexname){	
-	var obj = this;
-		return obj.createIndex(indexname);
-};
-
-// return index 
-Index.prototype.searchIndex = function (terms){	
-	var obj = this;
-	if(terms.length > 0){
-		terms.forEach(function(item){
-			if(obj.nindexes[indexname][item]) console.log;
-		});
-	}
-	else{if(obj.nindexes[indexname][terms]) console.log;}
-};
-
-// obtain name of JSON file for serializing index
-Index.prototype.getfilename = function(filepath){
-	if(filepath !== null){
-		return path.basename(filepath, path.extname(filepath));
-	}
-	else{return null;}
-};
-
-// reads the content ofthe JSON file, and builds index
-Index.prototype.readingFile = function (filepath, indexname, callback){
-	// binding object 
-	var obj = this;
-	async.waterfall([
-		function(concats){
-			fs.readFile(filepaths, function(err, data){
-				if (err) return console.log("error opening reading json");
-				if (data == '') return console.log("JSON file is empty");
-				try{
-					newdata = (JSON.parse(data));
-				}
-				catch(e){
-					return console.log(e.message);
-				}
-				concats(null, newdata, indexname);
-			});	
-		},
-
-		function(newdata, indexname, concats){
-			obj.jsondata[indexname] = newdata;
-			// initialize variables
-			index_all = '';
-			// concatenate content of each object to for building index
-			newdata.forEach(function(item, index){
-				// looping through each object in the data
-				// concatenate data into a single string (index_all)
-				for (var key in item){
-					index_all += ' ' + item[key];
-				}
-			});
-			concats(null);
-		}
-		], function concats(err){
-				if (err) return console.error(err);			
-				callback(index_all);
-  });	
-};
-
-
-function getTokens(index_all){
-	var tokens_i;	
-	// for full text search
-		tokens ='';
-		tokens_i = index_all.toLowerCase().split(/[.,\s]+/); //split by period, comma and spaces
-		tokener(tokens_i);
-}
-
-// sorts concatenated data and collect unique tokens
-function tokener(tokens_i){
-	tokens_i = tokens_i.filter(
-				function(tokens_i){
-					return tokens_i !== '';});
-			tokens_i = tokens_i.sort().filter(function(a,b,c){
-				return (b == c.indexOf(a)); });
-			if (typeof tokens !== 'string') tokens.push(tokens_i);
-			else tokens = tokens_i;
-}
-
-// generates index by group or full index
-function generateTokendata(obj, tokens, jsondata, indexname){
-		tokens.forEach(function(item, index){
-			obj.nindexes[indexname][item] = [];		
-			for(var keys in jsondata){
-				loopb:
-				for( var key in jsondata[keys]){
-						var Str = jsondata[keys][key].toLowerCase(); 
-					if (Str.indexOf(item) > -1){
-						obj.nindexes[indexname][item].push(keys);
-						break loopb;
-					}
-				}
-			}
-		});
-	}
-
-index1 = new Index();
-
-index1.getIndex('../../spec/jasmine_examples/books.json');
